2badebf328251a8d150d6f9af795766e99ae117f
Timur <148047715+h43wre534we42t@users.noreply.github.com>
2024-04-07 23:59:32 +0300

    Created reserve.html and modified app.py to try to make reservations possible

--- app.py
+++ app.py
@@ -34,4 +34,19 @@ def court(court_id):
     court = db.session.execute(sql, {"id":court_id}).fetchone()
     sql = text("SELECT * FROM reserved WHERE court_id = :court_id")
     reservations = db.session.execute(sql, {"court_id":court_id}).fetchall()
-    return render_template("court.html", court=court, reservations=reservations)
+    return render_template("court.html", court=court, reservations=reservations, court_id=court_id)
+
+@app.route("/reserve/<int:court_id>")
+def reserve(court_id):
+    return render_template("reserve", {"court_id":court_id})
+
+@app.route("/reservesend/<int:court_id>", methods=["POST"])
+def reservesend(court_id):
+    res_date = request.form['res_date']
+    res_start = request.form['res_start']
+    res_end = request.form['res_end']
+    reservee = request.form['reservee']
+    sql = text("INSERT INTO reserved (res_date, res_start, res_end, court_id, reservee) VALUES (:res_date, :res_start, :res_end, :court_id, :reservee)")
+    db.session.execute(sql, {"res_date":res_date, "res_start":res_start, "res_end":res_end, "reservee":reservee, "court_id":court_id})
+    db.session.commit()
+    return redirect("/")
